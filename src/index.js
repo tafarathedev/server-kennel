e0b40c42be87c113f4355b335a6dd2f7ea546319
Tafarathedev <tafaratakaiza19@gmail.com>
2023-03-02 08:44:31 -0800

    firsy

--- src/index.js
+++ src/index.js
@@ -1,49 +1,61 @@
-import express from 'express'
-import cors from  'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import bodyParser from 'body-parser'
-import './server/server.js'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import './server/server.js';
+
 //router pages
-import BlogRouter from './routes/BlogRouter.js'
-import UserRouter from './routes/UserRouter.js'
-import ProductRouter from './routes/ProductRouter.js'
-import DogsRouter from './routes/DogsRouter.js'
-import AdminUser from './routes/Admin/api/AdminRouter.js'
-import CartRouter from './routes/CartRouter.js'
-import Payment from './routes/payment.js'
+import BlogRouter from './routes/BlogRouter.js';
+import UserRouter from './routes/UserRouter.js';
+import ProductRouter from './routes/ProductRouter.js';
+import DogsRouter from './routes/DogsRouter.js';
+import AdminUser from './routes/Admin/api/AdminRouter.js';
+import CartRouter from './routes/CartRouter.js';
+import Payment from './routes/payment.js';
 
-
+dotenv.config();
 
 //express function
-const app = express() 
-//port 
-const port = process.env.PORT 
- //middlewares
-app.use(express.json())
+const app = express();
+
+//port
+const port = process.env.PORT;
+
+//middlewares
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: '*'
-}));
-
-
-
-
-
-
+app.use(
+  cors({
+    origin: '*', // Allow requests from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow the specified HTTP methods
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true // Allow credentials (e.g. cookies, authorization headers) to be included in requests
+  })
+);
 
 // router routes
-app.use(UserRouter)
-app.use(ProductRouter)
-app.use(CartRouter)
-app.use(DogsRouter)
-app.use(AdminUser)
-app.use(BlogRouter)
-app.use(Payment)
+app.use(UserRouter);
+app.use(ProductRouter);
+app.use(CartRouter);
+app.use(DogsRouter);
+app.use(AdminUser);
+app.use(BlogRouter);
+app.use(Payment);
+
 //checkout session
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
 
-
-
-
-app.listen(port, ()=>console.log('listening on '+port))
+// start the server
+app.listen(port, (err) => {
+  if (err) {
+    console.error(`Error starting server: ${err.message}`);
+  } else {
+    console.log(`Server listening on port ${port}`);
+  }
+});
--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,21 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.set("strictQuery", true)
+dotenv.config();
+
+mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser:true,
-}).then(() => console.log('Connected!'))
-.catch(err=>console.log(err.message))
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('Connected to MongoDB!');
+}).catch((err) => {
+  console.error(`Error connecting to MongoDB: ${err.message}`);
+  process.exit(1); // Exit the Node.js process with a non-zero exit code
+});
 
+mongoose.connection.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
 
-
-export default mongoose   
+export default mongoose;
