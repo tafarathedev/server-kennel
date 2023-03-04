28a36d751a6bc13071bfbc8fcc6930434b5d8443
Tafarathedev <tafaratakaiza19@gmail.com>
2023-03-04 00:46:41 -0800

    girst

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -5,40 +5,35 @@ import Stripe from 'stripe'
 
 
 //stripe here \
-const stripePromise  = new Stripe("sk_test51JRZ0KH8R1pU2TcbKCj97mQuZ8tZcAoerac8RMAg37KSJDx7EDeAq91O76WBDd5qwLqoEziOa2V6bzMTUdJy90cp00mSWQsN3C"); 
+const stripePromise  = new Stripe("sk_test_51JRZ0KH8R1pU2TcbKCj97mQuZ8tZcAoerac8RMAg37KSJDx7EDeAq91O76WBDd5qwLqoEziOa2V6bzMTUdJy90cp00mSWQsN3C"); 
 const stripe = await stripePromise;
+router.post('/api/checkout', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { name ,amount, currency, paymentMethodTypes, quantity,description, successUrl, cancelUrl } = req.body;
 
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types:paymentMethodTypes,
+      line_items: [
+        { 
+          name,
+          description,
+          amount,
+          currency,
+          quantity,
+        }
+      ],
+      success_url: successUrl,
+      cancel_url: cancelUrl
+    });
 
-
-
-router.post('/create-checkout-session', async (req, res) => {
-  const {products , cancelUrl , successUrl} = req.body;
-
-  const lineItems = products.map(product => {
-    return {
-      price_data: {
-        currency: 'ZMW',
-        product_data: {
-          name: product.name,
-          description: product.description,
-        },
-        unit_amount: product.price * 100,
-      },
-      quantity: product.quantity,
-    };
-  });
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: lineItems,
-    mode: 'payment',
-    success_url: successUrl,
-    cancel_url: cancelUrl,
-  });
-
-  res.json({ id: session.id });
+    res.json({ sessionId: session.id });
+      
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Unable to create checkout session' });
+  }
 });
-
 
 
 export default router
