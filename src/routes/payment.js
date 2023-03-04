86942af01c1c725ece739bee5aef3a482d0601af
Tafarathedev <tafaratakaiza19@gmail.com>
2023-03-04 01:04:11 -0800

    girst

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -9,17 +9,17 @@ const stripePromise  = new Stripe("sk_test_51JRZ0KH8R1pU2TcbKCj97mQuZ8tZcAoerac8
 const stripe = await stripePromise;
 router.post('/api/checkout', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const { name ,amount, currency, paymentMethodTypes, quantity,description, successUrl, cancelUrl } = req.body;
+  const { name ,amount, quantity,description, successUrl, cancelUrl } = req.body;
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types:paymentMethodTypes,
+      payment_method_types: ['card','alipay', 'wechat'],
       line_items: [
         { 
           name,
-          description,
+          description,  
           amount,
-          currency,
+          currency:'ZMW',
           quantity,
         }
       ],
@@ -37,3 +37,54 @@ router.post('/api/checkout', async (req, res) => {
 
 
 export default router
+
+
+/* import express from 'express';
+import Stripe from 'stripe';
+
+// Use express router
+const router = express.Router();
+
+// Stripe setup
+const stripe = new Stripe('sk_test_51JRZ0KH8R1pU2TcbKCj97mQuZ8tZcAoerac8RMAg37KSJDx7EDeAq91O76WBDd5qwLqoEziOa2V6bzMTUdJy90cp00mSWQsN3C');
+
+// Checkout endpoint
+router.post('/api/checkout', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const {
+    name,
+    amount,
+    currency,
+    paymentMethodTypes,
+    quantity,
+    description,
+    successUrl,
+    cancelUrl,
+  } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: paymentMethodTypes,
+      line_items: [
+        {
+          name,
+          description,
+          amount,
+          currency,
+          quantity,
+        },
+      ],
+      success_url: successUrl,
+      cancel_url: cancelUrl,
+    });
+
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Unable to create checkout session' });
+  }
+});
+
+export default router;
+ */
